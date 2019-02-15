1f9d78223178e6acd47f4058ec453336778d0436
Ahmed Sadman <ahmedsadman.211@gmail.com>
2019-02-15 16:22:45 +0600

    login and register route done

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const authRoutes = require('./routes/auth');
 const app = express();
 const port = process.env.PORT || 3000;
 
+/* ---------- Parser setup ---------- */
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(bodyParser.json());
 
+
+/* ----------- Routes ------------- */
 // authentication routes
 app.use('/auth', authRoutes);
+
 
 /* ---------- Server Settings ------------ */
 app.listen(port, () => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,25 @@
 exports.registerUser = (req, res, next) => {
     console.log(req.body);
     const response = {
-        status: 'OK',
-        result: [{
-            reply: 'true',
-            nid: '10111'
-        }]
+        status: 'Registration Successful',
+        reply: true,
+        nid: req.body.nid
     };
     res.send(response);
 };
 
 exports.loginUser = (req, res, next) => {
     console.log(req.body);
-    res.send({
-        status: 'OK'
-    });
+    const { nid, password } = req.body;
+    const response = {};
+    if (nid === '1234' && password === 'abcdefg') {
+        response.reply = true;
+        response.message = 'User Authenticated';
+        res.send(response);
+    } else {
+        response.reply = false;
+        response.message = 'Authentication failed';
+        res.status(400).send(response);
+    }  
 };
 
